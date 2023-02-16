c62ac13306579f76542f29a75e6e79616a9eb401
Saarah Khan <saarah_96@hotmail.co.uk>
2023-02-16 13:00:02 +0000

    1.removed about me section 2. added space facts section

---
+++ assets/js/facts.js
@@ -0,0 +1,16 @@
+const spaceFacts = [
+    "The first living creature to go into space was a dog named Laika.",
+    "The Great Red Spot on Jupiter is a storm that has been raging for at least 350 years.",
+    "The Apollo 11 mission that landed humans on the moon was launched on July 16, 1969.",
+    "The universe is estimated to be around 13.8 billion years old.",
+    "The sun is actually a star, and it's about 4.6 billion years old.",
+    "There are more possible iterations of a game of chess than there are atoms in the observable universe.",
+    "The temperature in the void of space is about minus 270.45 °C",
+  ];
+  
+  function generateFact() {
+    const factDiv = document.getElementById("fact");
+    const randomFact = spaceFacts[Math.floor(Math.random() * spaceFacts.length)];
+    factDiv.innerHTML = randomFact;
+  }
+  
